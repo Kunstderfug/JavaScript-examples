7fd63f162fd4a3d945324d70fa4eedc3b990f48c
Vyacheslav Gryaznov <slava.gryaznov@gmail.com>
2020-01-21 17:15:09 -0500

    updated

---
+++ snake/snake.js
@@ -0,0 +1,96 @@
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = 0;
+  this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
+  gameOver.textContent = '';
+
+  this.draw = function() {
+    ctx.fillStyle = 'green';
+
+    for (let i = 0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+    }
+
+    ctx.fillRect(this.x, this.y, scale, scale);
+  };
+
+  this.update = function() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+
+    this.tail[this.total - 1] = { x: this.x, y: this.y };
+
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if (this.x > canvas.width) {
+      this.x = 0;
+    }
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+    if (this.x < 0) {
+      this.x = canvas.width;
+    }
+    if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  };
+
+  this.changeDirection = function(direction) {
+    switch (direction) {
+      case 'Up':
+        this.xSpeed = 0;
+        this.ySpeed = -scale * 1;
+        break;
+      case 'Down':
+        this.xSpeed = 0;
+        this.ySpeed = scale * 1;
+        break;
+      case 'Left':
+        this.xSpeed = -scale * 1;
+        this.ySpeed = 0;
+        break;
+      case 'Right':
+        this.xSpeed = scale * 1;
+        this.ySpeed = 0;
+        break;
+      case 'p':
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        break;
+    }
+  };
+
+  this.eat = function(fruit) {
+    if (this.x === fruit.x && this.y === fruit.y) {
+      this.total++;
+      return true;
+    }
+    return false;
+  };
+
+  this.dead = function(death) {
+    for (let i = 0; i < this.tail.length; i++) {
+      if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+        console.log('GAME OVER!!');
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        this.total = 0;
+        this.tail = [];
+        gameOver.textContent = 'GAME OVER!!!';
+        newGame.textContent = 'Click here to start a new game';
+        newGame.onclick = function() {
+          gameOver.textContent = '';
+          newGame.textContent = '';
+          xSpeed = scale * 1;
+          ySpeed = 0;
+        };
+      }
+    }
+  };
+}
