c363604f182f9c592631aa582757137c2221cc4c
Vyacheslav Gryaznov <slava.gryaznov@gmail.com>
2020-02-11 14:28:55 -0500

    JS updated

--- High Functions/main.js
+++ High Functions/main.js
@@ -1,19 +1,31 @@
 const companies = [
-  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
-  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
-  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
-  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
-  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
-  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
-  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
-  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
-  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+  { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+  { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+  { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+  { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+  { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+  { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+  { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+  { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+  { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 }
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-const coSorted = companies.map(item => item.category);
-console.log(coSorted);
+// const companyName = companies.map(company => company.name + company.category);
+
+const companyNames = companies.map(
+  company =>
+    `The ${company.name} was created in ${company.start} and lasted till ${company.end}`
+);
+
+console.log(companyNames);
+// for (const company of companies) {
+//   console.log(company.name, company.start + '-' + company.end);
+// }
+
+// const coSorted = companies.map(item => item.category);
+// console.log(coSorted);
 
 // for (let i = 0; i < companies.length; i++) {
 //   console.log(companies[i]);
--- gallery-start/main.js
+++ gallery-start/main.js
@@ -5,9 +5,20 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+const newImage = images.map(SRC => {
+  const img = new Image();
+  img.src = 'images/' + SRC;
+  thumbBar.appendChild(img);
+
+  img.onclick = e => {
+    displayedImage.src = e.target.src;
+  };
+});
+
+btn.onclick = () => {
+  overlay.classList.toggle('show');
+};
 
 /* Wiring up the Darken/Lighten button */
